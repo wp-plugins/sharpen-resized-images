da32bbcad3f9f7dd9590d98a08bac3d25d47663b
unsalkorkmaz <unsalkorkmaz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 19:28:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sharpen-resized-images/tags/1.2@562638 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajx-sharpen-resized-images.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Sharpen Resized Images
+Plugin URI: http://unsalkorkmaz.com/ajx-sharpen-resized-images/
+Description: This plugin sharpening resized jpg image uploads in your WordPress. No settings required.
+Author: Ünsal Korkmaz
+Author URI: http://unsalkorkmaz.com/
+Version: 1.2
+*/ 
+
+
+function ajx_sharpen_resized_files( $resized_file ) {
+
+	$image = wp_load_image( $resized_file );
+	if ( !is_resource( $image ) )
+		return new WP_Error( 'error_loading_image', $image, $file );
+
+	$size = @getimagesize( $resized_file );
+	if ( !$size )
+		return new WP_Error('invalid_image', __('Could not read image size'), $file);
+	list($orig_w, $orig_h, $orig_type) = $size;
+
+	switch ( $orig_type ) {
+		case IMAGETYPE_JPEG:
+			$matrix = array(
+				array(-1.2, -1, -1.2),
+				array(-1, 20, -1),
+				array(-1.2, -1, -1.2),
+			);
+
+			$divisor = array_sum(array_map('array_sum', $matrix));
+			$offset = 0; 
+			imageconvolution($image, $matrix, $divisor, $offset);
+			imagejpeg($image, $resized_file,apply_filters( 'jpeg_quality', 90, 'edit_image' ));
+			break;
+		case IMAGETYPE_PNG:
+			return $resized_file;
+		case IMAGETYPE_GIF:
+			return $resized_file;
+	}
+
+	// we don't need images in memory anymore
+	imagedestroy( $image );
+	
+}	
+	
+add_filter('image_make_intermediate_size', 'ajx_sharpen_resized_files',900);
